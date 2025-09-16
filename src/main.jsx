cd602c13553a08579494d7146416d5662a930f4a
Bárbara Cabezas <barbara@Barbaras-MacBook-Air.local>
2025-09-16 00:40:19 -0300

    feat: add main view components, styles, and API functions

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
 import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from './views/Home'
+import MainPage from './views/Main'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <HomePage />
-  </StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/chats" element={<MainPage />} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
+);
