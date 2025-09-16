8e93a15b4cf52afa397e3b4926c20a52e206e5d5
Bárbara <barbara.fcp@uc.cl>
2025-09-16 18:38:55 -0300

    Revert "Fix/main view api"

--- src/components/EmptyState.jsx
+++
@@ -1,8 +0,0 @@
-export default function EmptyState({ message, spinner }) {
-  return (
-    <div className="empty">
-      {spinner && <div className="spinner" aria-hidden />}
-      <p>{message}</p>
-    </div>
-  );
-}
